d1990e12e592db1ca0526f7956276b4950bbe836
L1ndseyHerman <lherman21@jcu.edu>
2026-07-19 21:52:28 -0400

    Got MUI4 working, woo! Didn't need to downgrade other things!

--- src/components/Beach/Beach.tsx
+++ src/components/Beach/Beach.tsx
@@ -1,5 +1,13 @@
+import { Check } from "@material-ui/icons";
+
+//  Temporary <Check /> to make sure @material-ui/icons is working:
 function Beach() {
-  return <h1>Beach</h1>;
+  return (
+    <>
+      <h1>Beach</h1>
+      <Check />
+    </>
+  );
 }
 
 export default Beach;
--- src/components/DeepOcean/DeepOcean.tsx
+++ src/components/DeepOcean/DeepOcean.tsx
@@ -1,5 +1,13 @@
+import { Box } from "@material-ui/core";
+
+//  Brian from ShiftKey would yell at me if he caught me using this <Box/> :)
+//  It's just temporary to test if @material-ui/core is working, don't worry :)
 function DeepOcean() {
-  return <h1>DeepOcean</h1>;
+  return (
+    <Box>
+      <h1>DeepOcean</h1>
+    </Box>
+  );
 }
 
 export default DeepOcean;
