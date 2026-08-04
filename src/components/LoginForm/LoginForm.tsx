a521040b6d0779818f9c532577c1813ba823e5a2
L1ndseyHerman <lherman21@jcu.edu>
2026-08-03 21:51:40 -0400

    Baby steps towards login logic

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,13 +1,20 @@
-import { Button, TextField } from "@material-ui/core";
+import { Box, Button, TextField } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
 import { useIntl } from "react-intl";
+import { useState } from "react";
 
 export default function LoginForm() {
   const history = useHistory();
   const intl = useIntl();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleLoginButtonClick = () => {
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
     console.log("You clicked the login button!");
+    console.log(
+      "The username is: " + username + " and the password is: " + password,
+    );
     history.push("/beach");
   };
 
@@ -19,7 +26,7 @@ export default function LoginForm() {
           defaultMessage: "Login",
         })}
       </h1>
-      <div>
+      <Box component="form" onSubmit={handleSubmit}>
         <TextField
           id="username"
           label={intl.formatMessage({
@@ -27,6 +34,9 @@ export default function LoginForm() {
             defaultMessage: "Username",
           })}
           variant="outlined"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setUsername(e.target.value)
+          }
         />
         <TextField
           id="password"
@@ -35,18 +45,17 @@ export default function LoginForm() {
             defaultMessage: "Password",
           })}
           variant="outlined"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setPassword(e.target.value)
+          }
         />
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handleLoginButtonClick}
-        >
+        <Button variant="contained" color="primary" type="submit">
           {intl.formatMessage({
             id: "LoginForm.Login",
             defaultMessage: "Login",
           })}
         </Button>
-      </div>
+      </Box>
     </>
   );
 }
