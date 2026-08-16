75dcfc9ff326b4d5b2beb7c15a6f4c6a53030658
L1ndseyHerman <lherman21@jcu.edu>
2026-08-15 21:23:33 -0400

    The rest of the Cypress test files will be based on the roles. Here is the start of the ScubaDiver tests:

---
+++ cypress/e2e/logged-in-with-scuba-diver-role.cy.ts
@@ -0,0 +1,15 @@
+describe("logged-in-with-scuba-diver-role", () => {
+  it("redirests to the beach page", () => {
+    cy.visit("http://localhost:5173/beach-app");
+    cy.url().should("equal", "http://localhost:5173/beach-app#/login");
+
+    cy.get("input").eq(0).type("ScubaDiver");
+    cy.get("input").eq(1).type("0");
+    cy.get("button").click();
+
+    cy.url().should("equal", "http://localhost:5173/beach-app#/beach");
+    cy.get("h1").should("have.text", "Beach").should("be.visible");
+  });
+
+  //  TODO: Write more tests once you're done w the CSS for other components!
+});
