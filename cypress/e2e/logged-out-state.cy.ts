ecb76df0ff4087ea641aca729d0b30958b236c1a
L1ndseyHerman <lherman21@jcu.edu>
2026-08-27 17:31:51 -0400

    Added a phone-screen-size Cypress test for the logged-out-state.

--- cypress/e2e/logged-out-state.cy.ts
+++ cypress/e2e/logged-out-state.cy.ts
@@ -69,4 +69,39 @@ describe("logged-out-state", () => {
 
     cy.url().should("equal", "http://localhost:5173/beach-app#/login");
   });
+
+  it("looks good at a phone screen size too", () => {
+    //  This is my Samsung Galaxy S22's width in pixels, I forget the exact height:
+    cy.viewport(360, 700);
+
+    cy.visit("http://localhost:5173/beach-app");
+
+    cy.url().should("equal", "http://localhost:5173/beach-app#/login");
+
+    cy.get("h1").should("have.text", "Login").should("be.visible");
+
+    cy.get("p")
+      .should("have.text", "Looking for my other websites? Click Here.")
+      .should("be.visible");
+
+    cy.get("a").should("have.text", "Here.").should("be.visible");
+
+    cy.contains("That username and/or password is not in our system.").should(
+      "not.exist",
+    );
+
+    cy.get("label").should("have.length", 2);
+    cy.get("input").should("have.length", 2);
+
+    cy.get("label").eq(0).should("have.text", "Username").should("be.visible");
+    cy.get("input").eq(0).should("be.enabled");
+
+    cy.get("label").eq(1).should("have.text", "Password").should("be.visible");
+    cy.get("input").eq(1).should("be.enabled");
+
+    cy.get("button")
+      .should("have.text", "Login")
+      .should("be.visible")
+      .should("be.enabled");
+  });
 });
