7dcb73a2c90d1548c4e150b98102ea71fc13cb60
L1ndseyHerman <lherman21@jcu.edu>
2026-08-29 18:26:30 -0400

    Background image for the Beach page works! :D

--- src/components/BeachPage/BeachPage.tsx
+++ src/components/BeachPage/BeachPage.tsx
@@ -1,9 +1,28 @@
 import { FormattedMessage } from "react-intl";
+import { createStyles, makeStyles } from "@material-ui/core";
+import beachImage from "../../images/beach.jpg";
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    beachImage: {
+      backgroundImage: `url(${beachImage})`,
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover",
+      height: "100vh",
+      paddingTop: "5px",
+      borderRadius: "4px",
+    },
+  }),
+);
 
 export default function BeachPage() {
+  const classes = useStyles();
+
   return (
-    <h1>
-      <FormattedMessage id="BeachPage.Beach" defaultMessage="Beach" />
-    </h1>
+    <div className={classes.beachImage}>
+      <h1>
+        <FormattedMessage id="BeachPage.Beach" defaultMessage="Beach" />
+      </h1>
+    </div>
   );
 }
