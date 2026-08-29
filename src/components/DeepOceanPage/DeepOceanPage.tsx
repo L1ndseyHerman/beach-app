e9a87edb93f2952e1245f63bc43df05b7dc02174
L1ndseyHerman <lherman21@jcu.edu>
2026-08-29 19:28:44 -0400

    Done with background images! :D

--- src/components/DeepOceanPage/DeepOceanPage.tsx
+++ src/components/DeepOceanPage/DeepOceanPage.tsx
@@ -1,12 +1,33 @@
 import { FormattedMessage } from "react-intl";
+import { createStyles, makeStyles } from "@material-ui/core";
+import deepOceanImage from "../../images/deep_ocean.jpg";
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    deepOceanImage: {
+      backgroundImage: `url(${deepOceanImage})`,
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover",
+      height: "100vh",
+      paddingTop: "5px",
+      borderRadius: "4px",
+      //  The Deep Ocean needs dark mode:
+      color: "#FFFFFF",
+    },
+  }),
+);
 
 export default function DeepOceanPage() {
+  const classes = useStyles();
+
   return (
-    <h1>
-      <FormattedMessage
-        id="DeepOceanPage.DeepOcean"
-        defaultMessage="Deep Ocean"
-      />
-    </h1>
+    <div className={classes.deepOceanImage}>
+      <h1>
+        <FormattedMessage
+          id="DeepOceanPage.DeepOcean"
+          defaultMessage="Deep Ocean"
+        />
+      </h1>
+    </div>
   );
 }
--- src/components/ShallowOceanPage/ShallowOceanPage.tsx
+++ src/components/ShallowOceanPage/ShallowOceanPage.tsx
@@ -1,12 +1,31 @@
 import { FormattedMessage } from "react-intl";
+import { createStyles, makeStyles } from "@material-ui/core";
+import shallowOceanImage from "../../images/shallow_ocean.jpg";
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    shallowOceanImage: {
+      backgroundImage: `url(${shallowOceanImage})`,
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover",
+      height: "100vh",
+      paddingTop: "5px",
+      borderRadius: "4px",
+    },
+  }),
+);
 
 export default function ShallowOceanPage() {
+  const classes = useStyles();
+
   return (
-    <h1>
-      <FormattedMessage
-        id="ShallowOceanPage.ShallowOcean"
-        defaultMessage="Shallow Ocean"
-      />
-    </h1>
+    <div className={classes.shallowOceanImage}>
+      <h1>
+        <FormattedMessage
+          id="ShallowOceanPage.ShallowOcean"
+          defaultMessage="Shallow Ocean"
+        />
+      </h1>
+    </div>
   );
 }
